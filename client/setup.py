b501957578a5be8fd30b619da77c92802977079f
Domingo Dirutigliano <me@domy.sh>
2025-06-04 20:41:10 +0200

    more expasive regex on key for Store

--- client/setup.py
+++ client/setup.py
@@ -3,14 +3,20 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r', encoding='utf-8') as f:
-    required = [ele.strip() for ele in f.read().splitlines() if not ele.strip().startswith("#") and ele.strip() != ""]
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required = [
+        ele.strip()
+        for ele in f.read().splitlines()
+        if not ele.strip().startswith("#") and ele.strip() != ""
+    ]
 
 VERSION = "{{VERSION_PLACEHOLDER}}"
 
 setuptools.setup(
     name="exploitfarm",
-    version= VERSION if "{" not in VERSION else "0.0.0", #uv pip install -U . --no-cache-dir for testing
+    version=VERSION
+    if "{" not in VERSION
+    else "0.0.0",  # uv pip install -U . --no-cache-dir for testing
     author="Pwnzer0tt1",
     author_email="pwnzer0tt1@poliba.it",
     scripts=["xfarm"],
@@ -27,5 +33,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires=">=3.10",
 )
